2d876082a990c80ac1441f1987f58b8e0bdaf402
geoffreymamcer <115859560+geoffreymamcer@users.noreply.github.com>
2025-11-25 19:46:49 +0800

    fix

--- frontend/src/api/base_url.ts
+++ frontend/src/api/base_url.ts
@@ -1,4 +1,4 @@
 const BASE_URL = import.meta.env.PROD
-  ? "https://yourusername.pythonanywhere.com"
+  ? "https://markgeoffrey.pythonanywhere.com"
   : "http://127.0.0.1:8000";
 export default BASE_URL;
